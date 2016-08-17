4df9aed24e45d583d933e44dbec64db845dc8e8f
Adam Duston <adam.duston@ge.com>
2016-08-17 14:31:43 -0700

    Working form allows submitting details for redis server and running a benchmark. Displays results in a table. Probably needs more intensive testing and error handling functions, but has minimal functionality for now.

--- bin/benchmarks-ui.js
+++ bin/benchmarks-ui.js
@@ -7,13 +7,13 @@
 var express = require('express');
 var fs = require('fs');
 var exec = require('child_process').exec;
-var csv = require('csv');
-
+var RedisBenchmark = require('../lib/redis-benchmark');
+var bodyParser = require('body-parser');
 
 // Load the configuration from the config file.
 var configFile = "./config/config.json";
 try {
-var config = JSON.parse(fs.readFileSync(configFile));
+  var config = JSON.parse(fs.readFileSync(configFile));
 } catch (e) {
   // Quit if the config file can't be read (for now)
   console.log("Error parsing %s - %s", configFile, e);
@@ -26,23 +26,67 @@ if (config.hasOwnProperty("benchmark_cmd")) {
   benchmark_cmd = config.benchmark_cmd;
 }
 
-
 // Initialize the express application. Use Jade as the view engine
 var app = express();
-app.set('view engine','jade');
+app.set('view engine', 'jade');
 
 // Create a static resource for the public directory.
 app.use(express.static('public'));
 
+// Set some express configuration options
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // Default app route for index.hml
 app.get('/', function(req, res) {
-   res.render('index');
+  res.render('index');
 });
 
-var server = app.listen(config.port, config.host, function () {
-   var host = server.address().address;
-   var port = server.address().port;
+app.post('/', function(req, res) {
+  console.log(req.body);
+    benchmarkOpts = {
+      "redis_host": req.body.redis_host,
+      "redis_port": req.body.redis_port,
+      "redis_pw": req.body.redis_pw,
+      "num_requests": req.body.num_requests,
+      "benchmark_bin": benchmark_cmd
+    };
 
-   console.log('benchmarks-ui listening on %s:%s', host, port);
+    runBenchmark(benchmarkOpts, function(results) {
+      console.log(results);
+      res.render('results', {
+        "results": results,
+        "redis_host": req.body.redis_host,
+        "redis_port": req.body.redis_port,
+        "redis_pw": req.body.redis_pw,
+        "num_requests": req.body.num_requests
+        });
+    });
 });
+
+var server = app.listen(config.port, config.host, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('benchmarks-ui listening on %s:%s', host, port);
+
+});
+
+var runBenchmark = function(options, callback) {
+  /**
+   * Run a benchmark for the Redis server given in options.
+   */
+
+  // Assume the options sent are options appropriate for RedisBenchmark
+  benchmark = new RedisBenchmark(benchmarkOpts);
+
+  console.log("Running benchmark for %s:%s", options.redis_host, options.redis_port);
+
+  // Run the benchmark and pass the output to the calling function.
+  benchmark.run(options.num_requests, function(output){
+    callback(output);
+  });
+
+};
